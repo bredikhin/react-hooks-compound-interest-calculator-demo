5648775cd68cb23f449e1659c2de4a2c21d29a54
Ruslan Bredikhin <rus.brn@gmail.com>
2019-04-04 18:32:05 -0400

    add prettier

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,17 +1,22 @@
-import React, { memo, useState, useEffect } from 'react'
-import { withStyles, WithStyles, createStyles, Theme } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import Dinero from 'dinero.js'
+import React, { memo, useState, useEffect } from 'react';
+import {
+  withStyles,
+  WithStyles,
+  createStyles,
+  Theme
+} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Dinero from 'dinero.js';
 
 interface InputData {
-  principal: number,
-  interest: number,
-  annualAddition: number,
-  timesPerYear: number,
-  numberOfYears: number
+  principal: number;
+  interest: number;
+  annualAddition: number;
+  timesPerYear: number;
+  numberOfYears: number;
 }
 
 const defaults: InputData = {
@@ -20,7 +25,7 @@ const defaults: InputData = {
   annualAddition: 100,
   timesPerYear: 1,
   numberOfYears: 10
-}
+};
 
 const compound = ({
   principal,
@@ -29,75 +34,82 @@ const compound = ({
   timesPerYear,
   numberOfYears
 }: InputData): number => {
-  const k = 1 + interest / 100 / timesPerYear
+  const k = 1 + interest / 100 / timesPerYear;
 
-  return principal * Math.pow(k, numberOfYears) +
-    annualAddition * (Math.pow(k, numberOfYears + 1) - k)/(k - 1)
-}
+  return (
+    principal * Math.pow(k, numberOfYears) +
+    (annualAddition * (Math.pow(k, numberOfYears + 1) - k)) / (k - 1)
+  );
+};
 
-const styles = (theme: Theme) => createStyles({
-  root: {
-    minHeight: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    padding: '2em'
-  },
-  header: {
-    textTransform: 'uppercase'
-  },
-  field: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    flexBasis: 100
-  },
-  result: {
-    fontSize: '10vw'
-  }
-})
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      minHeight: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      padding: '2em'
+    },
+    header: {
+      textTransform: 'uppercase'
+    },
+    field: {
+      flex: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      flexBasis: 100
+    },
+    result: {
+      fontSize: '10vw'
+    }
+  });
 
 interface Props extends WithStyles<typeof styles> {
-  title: String
+  title: String;
 }
 
-const Calculator = ( { title, classes }: Props ) => {
-  const [principal, setPrincipal] = useState('')
-  const [interest, setInterest] = useState('')
-  const [annualAddition, setAnnualAddition] = useState('')
-  const [timesPerYear, setTimesPerYear] = useState('')
-  const [numberOfYears, setNumberOfYears] = useState('')
-  const [futureAmount, setFutureAmount] = useState('')
+const Calculator = ({ title, classes }: Props) => {
+  const [principal, setPrincipal] = useState('');
+  const [interest, setInterest] = useState('');
+  const [annualAddition, setAnnualAddition] = useState('');
+  const [timesPerYear, setTimesPerYear] = useState('');
+  const [numberOfYears, setNumberOfYears] = useState('');
+  const [futureAmount, setFutureAmount] = useState('');
 
   useEffect(() => {
-    setFutureAmount(compound({
-      principal: parseFloat(principal),
-      interest: parseFloat(interest),
-      annualAddition: parseFloat(annualAddition),
-      timesPerYear: parseInt(timesPerYear),
-      numberOfYears: parseInt(numberOfYears)
-    }).toString() || '')
-  })
+    setFutureAmount(
+      compound({
+        principal: parseFloat(principal),
+        interest: parseFloat(interest),
+        annualAddition: parseFloat(annualAddition),
+        timesPerYear: parseInt(timesPerYear),
+        numberOfYears: parseInt(numberOfYears)
+      }).toString() || ''
+    );
+  });
 
   return (
     <Grid container direction="column" className={classes.root}>
       <Grid item className={classes.field}>
-        {title && <header>
-          <Typography variant="subtitle1" className={classes.header}>
-            {title}
-          </Typography>
-        </header>}
+        {title && (
+          <header>
+            <Typography variant="subtitle1" className={classes.header}>
+              {title}
+            </Typography>
+          </header>
+        )}
       </Grid>
       <Grid item className={classes.field}>
         <TextField
           label="Principal"
           id="principal"
           InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            startAdornment: <InputAdornment position="start">$</InputAdornment>
           }}
           value={principal}
-          onChange={(e) => setPrincipal(e.target.value)}
+          onChange={e => setPrincipal(e.target.value)}
         />
       </Grid>
       <Grid item className={classes.field}>
@@ -106,10 +118,10 @@ const Calculator = ( { title, classes }: Props ) => {
           id="interest"
           className={classes.field}
           InputProps={{
-            startAdornment: <InputAdornment position="start">%</InputAdornment>,
+            startAdornment: <InputAdornment position="start">%</InputAdornment>
           }}
           value={interest}
-          onChange={(e) => setInterest(e.target.value)}
+          onChange={e => setInterest(e.target.value)}
         />
       </Grid>
       <Grid item className={classes.field}>
@@ -118,10 +130,10 @@ const Calculator = ( { title, classes }: Props ) => {
           id="annual-addition"
           className={classes.field}
           InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            startAdornment: <InputAdornment position="start">$</InputAdornment>
           }}
           value={annualAddition}
-          onChange={(e) => setAnnualAddition(e.target.value)}
+          onChange={e => setAnnualAddition(e.target.value)}
         />
       </Grid>
       <Grid item className={classes.field}>
@@ -130,7 +142,7 @@ const Calculator = ( { title, classes }: Props ) => {
           id="times-per-year"
           className={classes.field}
           value={timesPerYear}
-          onChange={(e) => setTimesPerYear(e.target.value)}
+          onChange={e => setTimesPerYear(e.target.value)}
         />
       </Grid>
       <Grid item className={classes.field}>
@@ -139,16 +151,18 @@ const Calculator = ( { title, classes }: Props ) => {
           id="number-of-years"
           className={classes.field}
           value={numberOfYears}
-          onChange={(e) => setNumberOfYears(e.target.value)}
+          onChange={e => setNumberOfYears(e.target.value)}
         />
       </Grid>
       <Grid item className={classes.field}>
         <Typography variant="h1" className={classes.result}>
-          {Dinero({ amount: Math.round((parseFloat(futureAmount) || 0) * 100)}).toFormat('$0,0.00')}
+          {Dinero({
+            amount: Math.round((parseFloat(futureAmount) || 0) * 100)
+          }).toFormat('$0,0.00')}
         </Typography>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default withStyles(styles)(memo(Calculator))
+export default withStyles(styles)(memo(Calculator));
